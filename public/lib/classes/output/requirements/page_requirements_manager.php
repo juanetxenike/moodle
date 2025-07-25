91556e0147f208af041d655ec45dd77bc1030a32
Andrew Nicols <andrew@nicols.co.uk>
2025-07-25 18:17:24 +0800

    MDL-87045 core: Add initial OpenTelemetry solution
    This commit provides changes required for our Open Telemetry
    integration.
    
    There are relatively few changes to the code itself, as most changes are
    performed by Composer packages which inspect Moodle from outside, as is
    typical with Open Telemetry.
    
    The changes that we do have here are:
    
    - introduction of a `\core\telemetry` class to provide helper methods
      for:
    
      - fetching the Trace ID of the current request's root span
      - fetching the Trace Parent ID of the current request's root span
      - checking if Open Telemetry is available
      - Recording Throwables
    
    - Recording Throwables from the exception handler
    - Display the Trace ID in the page footer
    - Adding the Trace Parent ID to the rendered page metadata
    - Add the Trace parent ID to requests made using the `core/fetch` and
      `core/ajax` modules.

--- public/lib/classes/output/requirements/page_requirements_manager.php
+++ public/lib/classes/output/requirements/page_requirements_manager.php
@@ -337,7 +337,7 @@ class page_requirements_manager {
                 'templaterev'           => $this->get_templaterev(),
                 'siteId'                => (int) SITEID,
                 'userId'                => (int) $USER->id,
-                'deprecationignorelist'       => !empty($CFG->jsdeprecationignorelist) ? $CFG->jsdeprecationignorelist : [],
+                'traceId'               => \core\telemetry::get_trace_parent_id(),
             ];
             if ($CFG->debugdeveloper) {
                 $this->M_cfg['developerdebug'] = true;
